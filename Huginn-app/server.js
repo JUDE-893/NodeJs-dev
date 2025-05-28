6ea9ac3d40c24e974fd586d2aa415f6d3ebcf280
JUDE-893 <ichbinarif01@gmail.com>
2025-05-28 21:28:46 +0100

    update: --feat - setup the http/ws hybrid server

--- Huginn-app/server.js
+++ Huginn-app/server.js
@@ -1,28 +1,41 @@
-import app from './index.js';
+import http from 'http';
+import { Server } from 'socket.io';
 import dotenv from 'dotenv'
-import mongoose from 'mongoose';
+import app from './index.js';
+import socketHandler from './socket/socket.js';
+import { connectDB } from './db/mongoose.js';
 
-dotenv.config()
+// LOAD ENVIRONEMET VARIABLES
+dotenv.config();
 
-const DB= process.env.DATABASE_LOCAL;
+// CONNECT TO DB
+connectDB();
 
-// connect
-mongoose.connect(DB, {
-  dbName: 'huginn',
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  writeConcern: {
-    w: 'majority',
-    j: true,
-    wtimeout: 1000
-}}).then(() => {
-  console.log('mongoose connected successfully');
-})
+// INITIATE SERVER
+const server = http.createServer(app);
 
-app.listen(process.env.PORT || 3000, () => {
+// INITIATE WEBSOCKET SERVER
+export const io = new Server(server, {
+  // cors: {
+  //   origin: process.env.CLIENT_SCHEME || 'http://localhost:3000',
+  //   methods: ['GET', 'POST'],
+  //   credentials: true
+  // }
+  cors: {
+   origin: "http://localhost:3000", // Match your React app's URL
+   methods: ["GET", "POST"],
+   credentials: true
+ },
+ allowEIO3: true
+});
+
+// SET WS EVENTS HANDLERS
+socketHandler(io).then()
+
+// START SERVER
+server.listen(process.env.PORT || 3000, () => {
   console.log(`Huginn app started on port..........${process.env.PORT}`);
-} )
-
+});
 
 /*
 cd Desktop\Dev\Next.Js-dev\huginn-app-project
